d5152c4a8c4aa7a28b4c6e806df82c1e81fdc529
uchitosato <uchitosato@gmail.com>
2023-06-15 07:36:24 +0900

    delete workflows

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
-# This script is meant to be run on Unix/Linux based systems
-set -e
+# #!/usr/bin/env bash
+# # This script is meant to be run on Unix/Linux based systems
+# set -e
 
-echo "*** Start Substrate node template ***"
+# echo "*** Start Substrate node template ***"
 
-cd $(dirname ${BASH_SOURCE[0]})/..
+# cd $(dirname ${BASH_SOURCE[0]})/..
 
-docker-compose down --remove-orphans
-docker-compose run --rm --service-ports dev $@
+# docker-compose down --remove-orphans
+# docker-compose run --rm --service-ports dev $@
